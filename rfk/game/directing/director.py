ba39b58a05788fd14e2e48306e739fde47670aae
damianhunt <dhuntsa@gmail.com>
2022-06-13 20:35:13 +0200

    commit

--- rfk/game/directing/director.py
+++ rfk/game/directing/director.py
@@ -17,6 +17,7 @@ class Director:
         """
         self._keyboard_service = keyboard_service
         self._video_service = video_service
+        self._total = 0
         
     def start_game(self, cast):
         """Starts the game using the given cast. Runs the main game loop.
@@ -58,9 +59,16 @@ class Director:
         
         for artifact in artifacts:
             if robot.get_position().equals(artifact.get_position()):
-                message = artifact.get_message()
-                banner.set_text(message)    
-        
+                ## Differentiate between rocks and gems to add or subtract points
+                if artifact.get_text() == '*':
+                    self._total = self._total + 1
+                    cast.remove_actor('artifacts', artifact)
+                    
+                else:
+                    self._total = self._total - 1
+                    cast.remove_actor('artifacts', artifact)
+                    
+            artifact.move_next(max_x, max_y)  
     def _do_outputs(self, cast):
         """Draws the actors on the screen.
         
--- rfk/game/services/keyboard_service.py
+++ rfk/game/services/keyboard_service.py
@@ -35,13 +35,13 @@ class KeyboardService:
         if pyray.is_key_down(pyray.KEY_RIGHT):
             dx = 1
         
-        if pyray.is_key_down(pyray.KEY_UP):
-            dy = -1
+        #if pyray.is_key_down(pyray.KEY_UP):
+        #    dy = -1
         
-        if pyray.is_key_down(pyray.KEY_DOWN):
-            dy = 1
+        #if pyray.is_key_down(pyray.KEY_DOWN):
+        #    dy = 1
 
-        direction = Point(dx, dy)
+        direction = Point(dx, 0)
         direction = direction.scale(self._cell_size)
         
         return direction
