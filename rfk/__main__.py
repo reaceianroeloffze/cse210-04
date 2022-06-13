fd973facf6978edeaf55e905855a5a11272fa2e0
damianhunt <dhuntsa@gmail.com>
2022-06-13 20:39:26 +0200

    commit

--- rfk/__main__.py
+++ rfk/__main__.py
@@ -24,7 +24,7 @@ ROWS = 40
 CAPTION = "Greed"
 DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
-DEFAULT_ARTIFACTS = 40
+DEFAULT_ARTIFACTS = 10
 
 
 def main():
