d9ef1e000ac11a6d4d06842f570331e4f00a6f68
damianhunt <dhuntsa@gmail.com>
2022-06-13 20:27:35 +0200

    commit

--- rfk/__main__.py
+++ rfk/__main__.py
@@ -21,7 +21,7 @@ CELL_SIZE = 15
 FONT_SIZE = 15
 COLS = 60
 ROWS = 40
-CAPTION = "Robot Finds Kitten"
+CAPTION = "Greed"
 DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
 DEFAULT_ARTIFACTS = 40
@@ -42,7 +42,7 @@ def main():
     
     # create the robot
     x = int(MAX_X / 2)
-    y = int(MAX_Y / 2)
+    y = int(MAX_Y - 15)
     position = Point(x, y)
 
     robot = Actor()
@@ -53,18 +53,14 @@ def main():
     cast.add_actor("robots", robot)
     
     # create the artifacts
-    with open(DATA_PATH) as file:
-        data = file.read()
-        messages = data.splitlines()
 
     for n in range(DEFAULT_ARTIFACTS):
-        text = chr(random.randint(33, 126))
-        message = messages[n]
-
+        text = random.choice(['*', '0'])
         x = random.randint(1, COLS - 1)
         y = random.randint(1, ROWS - 1)
         position = Point(x, y)
-        position = position.scale(CELL_SIZE)
+        position = position.scale(CELL_SIZE) 
+
 
         r = random.randint(0, 255)
         g = random.randint(0, 255)
@@ -76,8 +72,10 @@ def main():
         artifact.set_font_size(FONT_SIZE)
         artifact.set_color(color)
         artifact.set_position(position)
-        artifact.set_message(message)
+        artifact.set_velocity(Point(0,random.choice([3,5])))
         cast.add_actor("artifacts", artifact)
+
+        
     
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
